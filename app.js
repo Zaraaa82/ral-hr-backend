4ec41eb43d7a258fbb4d6e90be4eb4909216e1e0
Zahraa Alaiwi <zozoa.jalil@gmail.com>
2026-08-29 02:00:58 +0300

    feat: add leave request routes to app

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 
 // Routes Import
 const authRoutes = require('./routes/auth.routes')
+const leaveRequestRoutes = require('./routes/leaveRequest.routes');
 
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
@@ -24,6 +25,7 @@ app.use(morgan('dev'))
 
 // Routes
 app.use('/auth',authRoutes)
+app.use('/leave-requests', leaveRequestRoutes)
 
 
 
